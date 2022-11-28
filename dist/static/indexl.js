0a6e860a8a31fdd109c22b086b36056b4b5c6a5a
gavan <sp.gavan.zhang@enflame-tech.com>
2022-11-28 15:43:58 +0800

    更新

--- dist/static/indexl.js
+++ dist/static/indexl.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Shape|Shifter|Type|to start|#rectangle|#countdown 3||');
+      S.UI.simulate('Hi|妹妹|祝你|生日|快乐|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
