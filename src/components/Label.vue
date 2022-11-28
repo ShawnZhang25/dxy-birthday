1172784508228494ecb97caf59c100824e47cb60
gavan <sp.gavan.zhang@enflame-tech.com>
2022-11-28 15:21:44 +0800

    init

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="label">
-    <iframe width="100%" style="height: 100vh;border: none;" src="../assets/label.html"></iframe>
+    <iframe width="100%" style="height: 100vh;border: none;" src="https://shawnzhang25.github.io/dxy-birthday/dist/static/label.html"></iframe>
   </div>
 </template>
 
